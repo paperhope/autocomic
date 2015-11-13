2a2f708931ca33d02d72e58b706e5343119e952d
Haavard Futsaeter <futsaeter@gmail.com>
2015-11-13 21:11:05 +0100

    In the middle of fixing image search

--- autocomic/__init__.py
+++ autocomic/__init__.py
@@ -1,6 +1,7 @@
 import sys
-import Google-Search-API
-#import pyside
+import requests
+from google import google, images
+import googlesearch
 
 
 class AutoComic(object):
@@ -9,40 +10,64 @@ class AutoComic(object):
     """
     def __init__(self, script):
         self.script = script
-        self.panels = [Panel(text) for text in self.split_script_into_elements()]
+        self.panels = [GooglePanel(text) for text in self.split_script_into_elements()]
+        self.search = googlesearch.GoogleSearch(cx="017604795262263300250:cjpxtqbxtv0", key="AIzaSyBUoBTFf-KoF-w0qJOBgzWChP02hSjutJo" )
 
     def split_script_into_elements(self):
         
         return list(self.script)
 
-    def press(self):
-        for panel in self.panels:
-            print "something"
+    def get_good_art(self):
 
+        for panel in self.panels:
+            panel.find_art()
 
 class Panel(object):
 
     def __init__(self, text):
         self.text = text
 
-    def find_art(self, text):
+    def find_art(self):
         pass
 
-    def press(self)
+    def serialize(self):
+        pass
+
+
+class GooglePanel(Panel):
     
+    def __init__(self, text):
+        super(GooglePanel, self).__init__(text)
+        
+        self.google_options = images.ImageOptions()
+        self.google_options.size_category = images.SizeCategory.SMALL
+        
+    def find_art(self):
+        results  = google.search_images(self.text, self.google_options)
+        
+        self.art = self._get_image(results[0].link)
+
+        print "Image: %s" % self.art
+
+    def _get_image(self, url):
+        response = requests.get(url)
+        
+        response.raise_for_status()
+
+        return response.content
 
 def main():
     
-    print "Give me a story:"
+    print("Give me a story:")
 
     script = []
     for line in sys.stdin:
         script.append(line)
 
-    for line in script: print line
-
     autocomic = AutoComic(script)
-    autocomic.press()
-    
+    autocomic.get_good_art()
+
+    print "Number of panels: %s" % len(autocomic.panels)
+
 if __name__ == "__main__":
     main()
