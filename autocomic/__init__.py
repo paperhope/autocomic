1b045d7a9c3c3cd23038002b200efbccab7b596e
Haavard Futsaeter <futsaeter@gmail.com>
2015-11-16 19:00:11 +0100

    Skeleton classes for panel factory.

--- autocomic/__init__.py
+++ autocomic/__init__.py
@@ -1,6 +1,8 @@
 import sys
 import requests
-from google import google, images
+import ConfigParser
+import argparse
+
 import googlesearch
 
 
@@ -8,10 +10,13 @@ class AutoComic(object):
     """
     Contain text story and all the comic elements based on this story
     """
-    def __init__(self, script):
+    def __init__(self, script, panel_factory):
         self.script = script
-        self.panels = [GooglePanel(text) for text in self.split_script_into_elements()]
-        self.search = googlesearch.GoogleSearch(cx="017604795262263300250:cjpxtqbxtv0", key="AIzaSyBUoBTFf-KoF-w0qJOBgzWChP02hSjutJo" )
+        self.panel_factory = panel_factory
+        
+        self.panels = [self.panel_factory.create_panel(text) for text in 
+                       self.split_script_into_elements()]
+
 
     def split_script_into_elements(self):
         
@@ -21,6 +26,7 @@ class AutoComic(object):
 
         for panel in self.panels:
             panel.find_art()
+
 
 class Panel(object):
 
@@ -36,27 +42,59 @@ class Panel(object):
 
 class GooglePanel(Panel):
     
-    def __init__(self, text):
+    def __init__(self, text, search):
         super(GooglePanel, self).__init__(text)
-        
-        self.google_options = images.ImageOptions()
-        self.google_options.size_category = images.SizeCategory.SMALL
+        self.search = search
         
     def find_art(self):
-        results  = google.search_images(self.text, self.google_options)
-        
-        self.art = self._get_image(results[0].link)
+        self.art = search.get_image(self.text)
 
         print "Image: %s" % self.art
 
-    def _get_image(self, url):
-        response = requests.get(url)
-        
-        response.raise_for_status()
 
-        return response.content
+class PanelFactory(object):
+
+    def create_panel(self, text):
+        """
+        Method MUST be implemented in subclass. 
+        Receive text as argument and return a panel instance.
+        """
+        raise NotImplementedError
+
+
+class GooglePanelFactory(object):
+    """
+    Factory class for creating panels with images from google search.
+    """
+    def __init__(self, search):
+
+        self.search = search
+
+    def create_panel(self, text):
+
+        return GooglePanel(text, search)
+
+
+def config_values(config_file):
+    """
+    Get config values google_engine and client_key from config file.
+    Raise exception if config values is missing from config file.
+    """
+
+    config = ConfigParser.SafeConfigParser()
+    config.read(config_file)
+    
+    return (config.get('googlesearch', 'search_engine_id'), 
+            config.get('googlesearch', 'client_key'))
+
 
 def main():
+
+    parser.add_argument('--config', default='autocomic.ini',
+                   help='path to config file in ini format')
+
+    parser.parse_args()
+    google_engine, client_key = config_values(config)
     
     print("Give me a story:")
 
@@ -64,7 +102,8 @@ def main():
     for line in sys.stdin:
         script.append(line)
 
-    autocomic = AutoComic(script)
+    search = googlesearch.GoogleSearch(cx=google_engine, key=client_key)
+    autocomic = AutoComic(script, search)
     autocomic.get_good_art()
 
     print "Number of panels: %s" % len(autocomic.panels)
